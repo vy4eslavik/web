0ee624ac90c1b1ed8eadf9f0a8f5eabbbd266db2
vy4eslavik <vy4eslavik@yandex.ua>
2013-07-31 14:10:48 +0300

    Update

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-function resetTabs(){
-    $("#content div").hide();
-    $("#tabs a").attr("id","");
-}
-
-var myUrl = window.location.href; //Получаем URL
-var myUrlTab = myUrl.substring(myUrl.indexOf("#"));
-var myUrlTabName = myUrlTab.substring(0,4);
-
-(function(){
-    $("#content div").hide();
-    $("#tabs li:first a").attr("id","current");
-    $("#content div:first").fadeIn();
-
-    $("#tabs a").on("click",function(e) {
-        e.preventDefault();
-        if ($(this).attr("id") == "current"){
-            return
-        }
-        else{
-            resetTabs();
-            $(this).attr("id","current");
-            $($(this).attr('name')).fadeIn();
-        }
-    });
-
-    for (i = 1; i <= $("#tabs li").length; i++) {
-        if (myUrlTab == myUrlTabName + i) {
-            resetTabs();
-            $("a[name='"+myUrlTab+"']").attr("id","current");
-            $(myUrlTab).fadeIn();
+$("#tabs a").on("click", function (e) {
+    e.preventDefault();
+    $('#img img').hide("normal");
+    if ($(this).attr("id") == "current") {
+        return;
+    }
+    else {
+        $("#tabs a").attr("id", "");
+        $(this).attr("id", "current");
+        $("#content div").hide();
+        $($(this).attr('name')).fadeIn();
+        $('#contacts').animate({'margin-right': '0px'}, 500);
+        switch ($(this).attr('name')) {
+            case '#tab2':
+                $('#img').animate({'background-position': "-300px"}, 500);
+                break;
+            case '#tab3':
+                $('#img').animate({'background-position': "-600px"}, 500);
+                break;
+            case '#tab4':
+                $('#img').animate({'background-position': "-900px"}, 500);
+                $('#contacts').animate({'margin-right': '-300px'}, 500);
+                break;
+            default:
+                $('#img').animate({'background-position': "0px"}, 500);
+                break;
         }
     }
-})();
+});
+$("#studUl li a").on("click", function (e) {
+    e.preventDefault();
+    $('#img img').hide();
+    $($(this).attr("name")).fadeIn();
+});
